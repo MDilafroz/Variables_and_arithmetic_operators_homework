95b7828f726ecff0e27346d150cba0759194d3f4
Dilafro'z <100587111+MDilafroz@users.noreply.github.com>
2022-06-08 16:44:37 +0500

    Create Arithmetic_op02.py

---
+++ Arithmetic_op02.py
@@ -0,0 +1,12 @@
+# Create a variable "a"  and assign it the value of the int 3.
+
+# Create a variable "b"  and assign it the value of the int 4.
+
+# Create a variable called 'answer'.
+
+# Find the sum of a and b than assign to 'answer'
+
+a=int(3)
+b=int(4)
+answer=a+b
+print("answer:",answer)
