dda503cbed97787047ebd3717d16044c4f942406
Dilafro'z <100587111+MDilafroz@users.noreply.github.com>
2022-06-08 17:00:05 +0500

    Create Arithmetic_op03.py

---
+++ Arithmetic_op03.py
@@ -0,0 +1,11 @@
+# Create a variable "a"  and assign it the value of the int 6.
+
+# Create a variable "b"  and assign it the value of the int 2.
+
+# Create a variable called 'answer'.
+
+# find the difference of a and b then assign to "answer"
+a=int(6)
+b=int(2)
+answer=a-b
+print("answer:",answer)
